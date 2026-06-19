2bdba662994b87b637e7db5feca085ab762dba34
Adbel <141557073+Adbelkhan@users.noreply.github.com>
2026-06-19 07:01:57 +0000

    conversion of datatypes

---
+++ 01_Basics/01_Varibles.js
@@ -0,0 +1,25 @@
+const accountId = 144553   
+let accountEmail = "hittesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 // not allow
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+/*
+use "const"  and "let"
+prefer not to use var
+because of issue in block scope and functional scope 
+
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,66 @@
+"use strict"; // treate all js code as newer version 
+
+
+//aleart(3+3) // we are using nodejs not browser 
+
+//console.log(3 
+  //  +   
+  //   3) // code redeability that should be high 
+
+//console.log("hitesh")     
+
+
+//let name = "hitesh " // string 
+//let age = 18 
+//let isLoggedIn = false;
+//let state;
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+// boolean => true/false
+// null = is actually a standalone value 
+// undefined =>
+// symbol = > fro uniqueness 
+
+
+// object
+
+
+//console.log(typeof "Adbel");
+
+//console.log(typeof age);
+
+//console.log(typeof null);  // null ka type object hai always remember 
+
+
+
+//Question 1
+//Create variables of different data types and print their values and types.
+
+let name = "Adbel";
+let age = 21;
+let isStudent = true;
+let city ;
+let salary = null;
+
+console.log(name);  // value 
+console.log(typeof name);  // type 
+
+console.log(age);
+console.log(typeof age);
+
+console.log(isStudent);
+console.log(typeof isStudent);
+
+console.log(isStudent);
+console.log(typeof isStudent);
+
+console.log(city);
+console.log(typeof city);
+
+console.log(salary);
+console.log(typeof salary);
+
+
+
